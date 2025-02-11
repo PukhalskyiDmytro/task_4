b07db6dcac523744fe0937058ddd231ced91caeb
mykhailo678 <mykhailo@knu.ua>
2025-02-11 13:23:56 +0200

    add def is_prime

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from utils import *
 
 if __name__ == "__main__":
     print(find_factorial(6))
+    print(is_prime(121))
 
--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 def find_factorial(n):
     result = 1
     for i in range(1,n+1):
@@ -6,3 +15,5 @@ def find_factorial(n):
 
 if __name__ == "__main__":
     print(find_factorial(5))
+
+
